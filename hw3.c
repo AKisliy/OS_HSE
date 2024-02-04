6f8ac02b210b686457e6f0d29a0915c93eec41e0
AKisliy <lekha.a-kisel@yandex.ru>
2024-02-04 17:28:21 +0300

    Update hw3

--- hw3.c
+++ hw3.c
@@ -1,6 +1,55 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+
+unsigned long long max = ULLONG_MAX;
+
+void factorial(int a){
+    unsigned long long res = 1;
+    unsigned long long cur = 1;
+
+    while(cur <= a){
+        if(max / cur < res){
+            printf("Can't calculate full factorial, because of overflow.\n");
+            printf("The last found value is %llu! = %llu\n", cur - 1,res);
+            break;
+        }
+        res *= cur;
+        ++cur;
+    }
+    if(cur > a){
+        printf("%d! = %llu\n",a, res);
+    }
+}
+
+void fibonacci(int a){
+    if(a == 1){
+        printf("Fibonacci_1 = 0\n");
+    } else if(a == 2){
+        printf("Fibonacci_2 = 1\n");
+    } else{
+        unsigned long long prevprev = 0;
+        unsigned long long prev = 1;
+        int index = 3;
+        while(index <= a){
+            unsigned long long temp = prev;
+            if(max - prev < prevprev){
+                printf("Can't calculate Fibonacci, because of overflow.\n");
+                printf("The last found value is: Fibonacci_%d = %llu\n",index - 1, prev);
+                break;
+            }
+            prev = prev + prevprev;
+            prevprev = temp;
+            ++index;
+        }
+        if(index > a){
+            printf("Fibonacci_%d = %llu\n", a, prev);
+        }
+    }
+}
 
 int main(int argc, char *argv[]){
     if(argc == 1){
@@ -14,8 +63,6 @@ int main(int argc, char *argv[]){
         return 0;
     }
     
-    unsigned long long max = ULLONG_MAX;
-    
     pid_t pid, ppid, chpid;
     chpid = fork();
     pid = getpid();
@@ -25,48 +72,20 @@ int main(int argc, char *argv[]){
     
     if(chpid <= -1){
         printf("Something went wrong with fork!\n");
-    } else if(chpid == 0){
-        unsigned long long res = 1;
-        unsigned long long cur = 1;
-
-        while(cur <= a){
-            if(max / cur < res){
-                printf("Can't calculate full factorial, because of overflow.\n");
-                printf("The last found value is %llu! = %llu\n", cur - 1,res);
-                break;
-            }
-            res *= cur;
-            ++cur;
-        }
-        if(cur > a){
-            printf("%d! = %llu\n",a, res);
-        }
+    } else if(chpid == 0){ // child will calculate factorial
+        factorial(a);
         printf("\n");
-    } else{
-        if(a == 1){
-            printf("Fibonacci_1 = 0\n");
-        } else if(a == 2){
-            printf("Fibonacci_2 = 1\n");
-        } else{
-            unsigned long long prevprev = 0;
-            unsigned long long prev = 1;
-            int index = 3;
-            while(index <= a){
-                unsigned long long temp = prev;
-                if(max - prev < prevprev){
-                    printf("Can't calculate Fibonacci, because of overflow.\n");
-                    printf("The last found value is: Fibonacci_%d = %llu\n",index - 1, prev);
-                    break;
-                }
-                prev = prev + prevprev;
-                prevprev = temp;
-                ++index;
-            }
-            if(index > a){
-                printf("Fibonacci_%d = %llu\n", a, prev);
-            }
-        }
+    } else{ // parent will calculate a-th Fibonacci number
+        fibonacci(a);
         printf("\n");
+        pid_t ls_pid = fork(); // create new process to show directory
+        if(ls_pid == 0){
+            printf("Here your directory info: \n");
+            execlp("ls","ls","-al", NULL);
+            exit(0);
+        } else if(ls_pid < 0){
+            printf("Something went wrong with fork!\n");
+        }
     }
     return 0;
 }
